161ab3beb737cbb9f0c9e12bc98a5872531c7401
jaydubfilm <jaydubfilm@gmail.com>
2019-12-07 18:09:05 -0500

    green plus fade
    move green pluses

--- Assets/Scripts/Repair.cs
+++ Assets/Scripts/Repair.cs
@@ -36,14 +36,46 @@ public class Repair : MonoBehaviour
     public void HealTarget(){
         if (target!=null){
             Brick targetBrick = target.GetComponent<Brick>();
-            GameObject newHealSymbol = Instantiate(healingSymbol,targetBrick.transform);
-            StartCoroutine(WaitToEraseHealSymbol(newHealSymbol));
+            GameObject newHealSymbol = Instantiate(healingSymbol,gameObject.transform);
+            
+            StartCoroutine(MoveSymbol(newHealSymbol));
+            StartCoroutine(FadeOutSymbol(newHealSymbol));
 
             targetBrick.AdjustHP(healPower[brick.brickLevel]);
             if (targetBrick.brickHP>=targetBrick.brickMaxHP[targetBrick.brickLevel]) {
                 targetBrick.brickHP = targetBrick.brickMaxHP[targetBrick.brickLevel];
                 // target = FindNewTarget();
             }
+        }
+    }
+
+    IEnumerator MoveSymbol (GameObject symbol) {
+        Transform originalPos = gameObject.transform;
+        Vector3 newPos = target.GetComponent<Rigidbody2D>().transform.position;
+        float duration = (newPos-originalPos.position).magnitude/2.0f;
+        float t = 0.0f;
+        while (t< duration)
+            {
+                symbol.transform.position = Vector3.Lerp(originalPos.position,newPos,t/duration);
+                yield return null;
+                t+=Time.deltaTime;
+            }
+            symbol.transform.position = newPos;
+           // Destroy(symbol);
+    }
+
+    IEnumerator FadeOutSymbol(GameObject symbol){
+        SpriteRenderer spriteR = symbol.GetComponent<SpriteRenderer>();
+
+        Color tmpColor = spriteR.color;
+        float fadeTime = 1.0f;
+        while (tmpColor.a > 0f) {
+            tmpColor.a -= Time.deltaTime / fadeTime;
+            spriteR.color = tmpColor;
+            if (tmpColor.a <=0)
+                tmpColor.a = 0;
+            yield return null;
+            spriteR.color = tmpColor;
         }
     }
 
