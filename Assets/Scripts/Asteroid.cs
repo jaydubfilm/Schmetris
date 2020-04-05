47a40f252d777c32073417e7f500fe6050f31af1
Megan Kuhrt <mkuhrt0329@gmail.com>
2020-04-05 18:18:30 -0400

    Fixed asteroid destruction behaviour

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -6,22 +6,20 @@ public class Asteroid : MonoBehaviour
 {
 
     public int hP;
-    Block block;
+    Bit bit;
+
     // Start is called before the first frame update
     void Start()
     {
-        block = GetComponent<Block>();
+        bit = GetComponent<Bit>();
     }
 
     public void AdjustHP(int damage, Transform bullet)
     {
-        print("hit asteroid") ;
-
-        hP -= damage;        
-        if(hP <= 0)
+        hP -= damage;
+        if (hP <= 0)
         {
-            block.DestroyBlock();
+            bit.RemoveFromBlock("explode");
         }
-            Destroy(bullet.gameObject);
     }
 }
