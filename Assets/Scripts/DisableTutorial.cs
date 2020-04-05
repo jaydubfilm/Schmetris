c23b9ef1522920389eba3a28915086d47e7389b6
cspid <carlospidla@gmail.com>
2020-04-05 03:17:43 -0400

    Disabled tutorial on Medium and hard difficulties

---
+++ Assets/Scripts/DisableTutorial.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableTutorial : MonoBehaviour
+{
+
+
+
+    public GameObject tutorialManager;
+    public GameObject tutorialPanel;
+
+    
+    // Update is called once per frame
+    public void RemoveTutorial()
+    {
+       if(tutorialManager != null)
+            Destroy(tutorialManager);
+       if(tutorialPanel != null)
+            Destroy(tutorialPanel);
+
+    }
+}
