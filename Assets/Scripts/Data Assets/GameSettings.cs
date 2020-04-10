5907f3fa4c28bc92133eaa1e1289601ad5e5c93e
Megan Kuhrt <mkuhrt0329@gmail.com>
2020-04-10 01:21:32 -0400

    Cleanup pass on utility scripts

--- Assets/Scripts/Data Assets/GameSettings.cs
+++ Assets/Scripts/Data Assets/GameSettings.cs
@@ -1,30 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+//Overall app settings
 [CreateAssetMenu(fileName = "New Game Settings", menuName = "GameSettings")]
 public class GameSettings : ScriptableObject
 {
+    //Game function specifications
     public bool Schmetris; 
     public bool OrphanFall = true;
+
+    //Cell size
     public float colSize = 1.4f;
     public float rowSize = 1.4f;
+
+    //Game area size
     public int screenRadius = 20;
     public int rows = 20;
     public float topEdgeOfWorld = 40;
     public float bottomEdgeOfWorld = -20;
+
+    //Background image size
     public Sprite bgSprite;
     public float bgHeight = 278;
     public float bgWidth = 388;
     public float bgZDepth = 400;
-    public float bgScrollSpeed = 0.1f;
-    public Vector3 bgScale = new Vector3(21,21,1);
+    public Vector3 bgScale = new Vector3(21, 21, 1);
+
+    //Bot size
     public int maxBotRadius = 6;
-    public float ghostMoveSpeed = 30f;
     public int blockRadius = 3;
+
+    //Speed settings
+    public float bgScrollSpeed = 0.1f;
+    public float ghostMoveSpeed = 30f;
     public float[] speedLevels = new float[] { 0.5f, 0.75f, 1.0f, 1.5f, 2.0f };
     public int defaultSpeedLevel = 2;
 
+    //Market prices of base resources
     public MarketLevelData reddite;
     public MarketLevelData blueSalt;
     public MarketLevelData greenAlgae;
--- Assets/Scripts/Data Assets/SpeciesData.cs
+++ Assets/Scripts/Data Assets/SpeciesData.cs
@@ -1,17 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+//Controls stats for enemy types
 [CreateAssetMenu(fileName = "New Enemy", menuName = "Enemy")]
 public class SpeciesData : ScriptableObject
 {
+    //Integer used by bot to identify the type of enemy and differentiate brick-enemies like parasites from player bricks
     public int type;
+
+    //Enemy stats
     public int maxHP;
     public float attackRate;
     public int damage;
     public float speed;
 
-    //Resource yields on destruction
+    //Resources the player gains when the enemy is destroyed
     public int redYield = 0;
     public int blueYield = 0;
     public int yellowYield = 0;
