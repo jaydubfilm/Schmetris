78dc4a913dbd9a84c4b6b8a0a298fa2c9827baff
Alex Shulman <shulmanbusiness@gmail.com>
2020-06-10 14:12:22 -0400

    Stage 1 of implementing AnalyticEvents, with a static class and 10 example analyticEventTypes.

---
+++ Assets/Scripts/Utilities/AnalyticsManager.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+namespace StarSalvager.Utilities
+{
+    public static class AnalyticsManager
+    {
+        public enum AnalyticsEventType
+        {
+            GameStart,
+            GameOver,
+            LevelStart,
+            LevelComplete,
+            TutorialStart,
+            TutorialStep,
+            TutorialComplete,
+            TutorialSkip,
+            MissionUnlock,
+            MissionComplete,
+            None
+        }
+
+        public static bool ReportAnalyticsEvent(AnalyticsEventType eventType, object eventDataParameter = null, Dictionary<string, object> eventDataDictionary = null)
+        {
+            AnalyticsResult? result = null;
+            
+            bool analyticsResultSuccessful = false;
+            switch(eventType)
+            {
+                case AnalyticsEventType.GameStart:
+                    result = ReportGameStart(eventDataDictionary);
+                    break;
+                case AnalyticsEventType.GameOver:
+                    result = ReportGameOver(eventDataParameter.ToString(), eventDataDictionary);
+                    break;
+                case AnalyticsEventType.LevelStart:
+                    result = ReportLevelStart(eventDataParameter.ToString(), eventDataDictionary);
+                    break;
+                case AnalyticsEventType.LevelComplete:
+                    result = ReportLevelComplete(eventDataParameter.ToString(), eventDataDictionary);
+                    analyticsResultSuccessful = false;
+                    break;
+                case AnalyticsEventType.TutorialStart:
+                    result = ReportTutorialStart(eventDataDictionary);
+                    break;
+                case AnalyticsEventType.TutorialStep:
+                    result = ReportTutorialStep((int)eventDataParameter, eventDataDictionary);
+                    break;
+                case AnalyticsEventType.TutorialComplete:
+                    result = ReportTutorialComplete(eventDataDictionary);
+                    break;
+                case AnalyticsEventType.TutorialSkip:
+                    result = ReportTutorialSkip(eventDataDictionary);
+                    break;
+                case AnalyticsEventType.MissionUnlock:
+                    result = ReportMissionUnlock(eventDataDictionary);
+                    break;
+                case AnalyticsEventType.MissionComplete:
+                    result = ReportMissionComplete(eventDataDictionary);
+                    break;
+                case AnalyticsEventType.None:
+                    analyticsResultSuccessful = false;
+                    break;
+            }
+
+            if (result != null)
+            {
+                Debug.Log(eventType.ToString() + " with result of " + result);
+            }
+
+            return (result != null && result == AnalyticsResult.Ok);
+        }
+
+        private static AnalyticsResult ReportGameStart(Dictionary<string, object> eventData = null)
+        {
+            return AnalyticsEvent.GameStart(eventData);
+        }
+
+        private static AnalyticsResult ReportGameOver(string levelName, Dictionary<string, object> eventData = null)
+        {
+            return AnalyticsEvent.GameOver(levelName, eventData);
+        }
+
+        private static AnalyticsResult ReportLevelStart(string levelName, Dictionary<string, object> eventData = null)
+        {
+            return AnalyticsEvent.LevelStart(levelName, eventData);
+        }
+
+        private static AnalyticsResult ReportLevelComplete(string levelName, Dictionary<string, object> eventData = null)
+        {
+            return AnalyticsEvent.LevelComplete(levelName, eventData);
+        }
+
+        private static AnalyticsResult ReportTutorialStart(Dictionary<string, object> eventData = null)
+        {
+            return AnalyticsEvent.TutorialStart(eventData: eventData);
+        }
+
+        private static AnalyticsResult ReportTutorialStep(int tutorialStepIndex, Dictionary<string, object> eventData = null)
+        {
+            return AnalyticsEvent.TutorialStep(tutorialStepIndex, eventData: eventData);
+        }
+
+        private static AnalyticsResult ReportTutorialComplete(Dictionary<string, object> eventData = null)
+        {
+            return AnalyticsEvent.TutorialComplete(eventData: eventData);
+        }
+
+        private static AnalyticsResult ReportTutorialSkip(Dictionary<string, object> eventData = null)
+        {
+            return AnalyticsEvent.TutorialSkip(eventData: eventData);
+        }
+
+        private static AnalyticsResult ReportMissionUnlock(Dictionary<string, object> eventData = null)
+        {
+            return AnalyticsEvent.Custom("mission_unlock", eventData);
+        }
+
+        private static AnalyticsResult ReportMissionComplete(Dictionary<string, object> eventData = null)
+        {
+            return AnalyticsEvent.Custom("mission_complete", eventData);
+        }
+    }
+}
---
+++ Assets/Scripts/Utilities/AnalyticsManagerTestingScriptTemporary.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StarSalvager.Utilities;
+
+namespace StarSalvager
+{
+    public class AnalyticsManagerTestingScriptTemporary : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (AnalyticsManager.ReportAnalyticsEvent(AnalyticsManager.AnalyticsEventType.GameStart))
+            {
+                print("Event sent successfully");
+            }
+            else
+            {
+                print("Event failed");
+            }
+
+
+            if (AnalyticsManager.ReportAnalyticsEvent(AnalyticsManager.AnalyticsEventType.GameOver, 1.4f))
+            {
+                print("Event sent successfully");
+            }
+            else
+            {
+                print("Event failed");
+            }
+
+
+            if (AnalyticsManager.ReportAnalyticsEvent(AnalyticsManager.AnalyticsEventType.LevelStart, 1.4f))
+            {
+                print("Event sent successfully");
+            }
+            else
+            {
+                print("Event failed");
+            }
+
+
+            if (AnalyticsManager.ReportAnalyticsEvent(AnalyticsManager.AnalyticsEventType.LevelComplete, 1.4f))
+            {
+                print("Event sent successfully");
+            }
+            else
+            {
+                print("Event failed");
+            }
+
+
+            if (AnalyticsManager.ReportAnalyticsEvent(AnalyticsManager.AnalyticsEventType.TutorialStart))
+            {
+                print("Event sent successfully");
+            }
+            else
+            {
+                print("Event failed");
+            }
+
+
+            if (AnalyticsManager.ReportAnalyticsEvent(AnalyticsManager.AnalyticsEventType.TutorialStep, 1))
+            {
+                print("Event sent successfully");
+            }
+            else
+            {
+                print("Event failed");
+            }
+
+
+            if (AnalyticsManager.ReportAnalyticsEvent(AnalyticsManager.AnalyticsEventType.TutorialComplete))
+            {
+                print("Event sent successfully");
+            }
+            else
+            {
+                print("Event failed");
+            }
+
+            if (AnalyticsManager.ReportAnalyticsEvent(AnalyticsManager.AnalyticsEventType.TutorialSkip))
+            {
+                print("Event sent successfully");
+            }
+            else
+            {
+                print("Event failed");
+            }
+
+            if (AnalyticsManager.ReportAnalyticsEvent(AnalyticsManager.AnalyticsEventType.MissionUnlock))
+            {
+                print("Event sent successfully");
+            }
+            else
+            {
+                print("Event failed");
+            }
+
+            if (AnalyticsManager.ReportAnalyticsEvent(AnalyticsManager.AnalyticsEventType.MissionComplete))
+            {
+                print("Event sent successfully");
+            }
+            else
+            {
+                print("Event failed");
+            }
+        }
+    }
+}
