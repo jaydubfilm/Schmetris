00f0fe92efd6f228d393467c6b202848b2928145
Alex Shulman <shulmanbusiness@gmail.com>
2020-06-11 12:38:43 -0400

    Step 1 of experimenting with an obstacle avoidance system based off of vector fields

---
+++ Assets/Scripts/AI/Testing.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StarSalvager
+{
+    public class Testing : MonoBehaviour
+    {
+        public GameObject[] m_obstacles;
+        public GameObject[] m_agents;
+        
+        void Start()
+        {
+            m_obstacles = new GameObject[5];
+
+            for (int i = 0; i < m_obstacles.Length; i++)
+            {
+                GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                m_obstacles[i] = sphere;
+            }
+
+            m_obstacles[0].transform.position = new Vector2(0, 0);
+            m_obstacles[1].transform.position = new Vector2(-2, -1);
+            m_obstacles[2].transform.position = new Vector2(4, 1);
+            m_obstacles[3].transform.position = new Vector2(-1, -4);
+            m_obstacles[4].transform.position = new Vector2(2, 3);
+
+            m_agents = new GameObject[1];
+
+            for (int i = 0; i < m_agents.Length; i++)
+            {
+                GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                m_agents[i] = sphere;
+            }
+
+            m_agents[0].transform.position = new Vector2(-10, 0);
+        }
+
+        void Update()
+        {
+            Vector3 destination = new Vector3(10, 0);
+            float velocity = 1.25f;
+            
+            for (int i = 0; i < m_agents.Length; i++)
+            {
+                Vector2 direction = new Vector2(destination.x - m_agents[i].transform.position.x, destination.y - m_agents[i].transform.position.y);
+                direction.Normalize();
+                direction += calculateForceAtPoint(m_agents[i].transform.position, m_obstacles);
+                direction.Normalize();
+                direction *= velocity;
+                Vector3 vec3Direction = direction;
+                m_agents[i].transform.position = m_agents[i].transform.position + (vec3Direction * Time.deltaTime);
+            }
+        }
+
+        private Vector2 calculateForceAtPoint(Vector2 agentPosition, GameObject[] obstacles)
+        {
+            Vector2 force = new Vector2(0, 0);
+            foreach (GameObject obstacle in obstacles)
+            {
+                Vector2 obstacleForce = getForce(agentPosition, obstacle.transform.position);
+                force.x += obstacleForce.x;
+                force.y += obstacleForce.y;
+            }
+            return force;
+        }
+
+        private Vector2 getForce(Vector2 agentPosition, Vector2 obstaclePosition)
+        {
+            /*if (Mathf.Abs(agentPosition.x - obstaclePosition.x) + Mathf.Abs(agentPosition.y - obstaclePosition.y) > 4)
+            {
+                return new Vector2(0, 0);
+            }*/
+
+            float magnitude = 1 / Vector2.SqrMagnitude(obstaclePosition - agentPosition);
+            Vector2 direction = new Vector2(agentPosition.x - obstaclePosition.x, agentPosition.y - obstaclePosition.y);
+            direction.Normalize();
+            direction *= magnitude;
+            return direction;
+        }
+    }
+}
---
+++ Assets/Scripts/AI/TestingGrid.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StarSalvager
+{
+    public class TestingGrid
+    {
+        private int m_width;
+        private int m_height;
+        private float m_gridCellSize;
+        private int[,] m_gridArray;
+
+        public TestingGrid(int width, int height, float gridCellSize)
+        {
+            m_width = width;
+            m_height = height;
+            m_gridCellSize = gridCellSize;
+
+            m_gridArray = new int[m_width, m_height];
+
+            for (int x = 0; x < m_gridArray.GetLength(0); x++)
+            {
+                for (int y = 0; y < m_gridArray.GetLength(1); y++)
+                {
+                    //GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                    //sphere.transform.position = 
+                    Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), new Color(255, 0, 0), 300f);
+                    Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), new Color(255, 0, 0), 300f);
+                }
+            }
+            Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), new Color(255, 0, 0), 300f);
+            Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), new Color(255, 0, 0), 300f);
+        }
+
+        private Vector2 GetWorldPosition (int x, int y)
+        {
+            return new Vector2(x, y) * m_gridCellSize;
+        }
+
+        public Vector2 GetArrayPositionOfVector(Vector2 worldLocation)
+        {
+            return new Vector2((int) (worldLocation.x / m_gridCellSize), (int) (worldLocation.y / m_gridCellSize));
+        }
+    }
+}
