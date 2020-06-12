5ef77fd6e6c5bdf2b705ca6befb8e5d19e09a6d3
Alex Bedard-Reid <alexbedardreid@gmail.com>
2020-06-12 13:22:41 -0400

    TMP Essentials Added & Prototype Scene
    [ADD] Added a new scene which will be used to jump between Different Demos to showcase work using a build
    [ADD] Added the TextMeshPro Essential files so that it all functions correctly
    [MOD] Added the Protoype Menu Scene
    [MOD] Reorderd scene to make more sense for demo

---
+++ Assets/Scripts/Prototyping/SceneLoaderPrototype.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace StarSalvager.Prototype
+{
+    public class SceneLoaderPrototype : MonoBehaviour
+    {
+        [Serializable]
+        public class SceneData
+        {
+            [SerializeField]
+            private int SceneIndex;
+            [SerializeField]
+            private string SceneTitle;
+            [Required]
+            [SerializeField]
+            private Button button;
+
+            public void Init()
+            {
+                button.GetComponentInChildren<TMP_Text>().text = SceneTitle;
+            
+                button.onClick.AddListener(() =>
+                {
+                    SceneManager.LoadScene(SceneIndex);
+                });
+            }
+        
+        }
+
+        [SerializeField]
+        private SceneData[] _sceneDatas;
+
+        [SerializeField, Required]
+        private GameObject SceneSelectionWindowObject;
+
+        [SerializeField]
+        private Button resetButton;
+        [SerializeField]
+        private Button returnToMenuButton;
+        // Start is called before the first frame update
+        private void Start()
+        {
+            DontDestroyOnLoad(gameObject);
+            DontDestroyOnLoad(EventSystem.current.gameObject);
+            
+            resetButton.gameObject.SetActive(false);
+            returnToMenuButton.gameObject.SetActive(false);
+            
+            
+            foreach (var sceneData in _sceneDatas)
+            {
+                sceneData.Init();
+            }
+            
+            resetButton.onClick.AddListener(() =>
+            {
+                var index = SceneManager.GetActiveScene().buildIndex;
+                SceneManager.LoadScene(index);
+            });
+            
+            returnToMenuButton.onClick.AddListener(() =>
+            {
+                SceneManager.LoadScene(0);
+            });
+
+            SceneManager.sceneLoaded += (scene, mode) =>
+            {
+                SceneSelectionWindowObject.SetActive(scene.buildIndex == 0);
+                resetButton.gameObject.SetActive(scene.buildIndex != 0);
+                returnToMenuButton.gameObject.SetActive(scene.buildIndex != 0);
+            };
+        }
+    }
+}
+
