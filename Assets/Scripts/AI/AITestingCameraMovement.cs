43de179fc0b3a9cf9945f27b14c03fab0074e111
Alex Shulman <shulmanbusiness@gmail.com>
2020-06-12 14:30:01 -0400

    Variable renaming pass, code cleanup and optimization, and the creation of falling obstacles and modification to tracking which squares of the grid obstacles are in.

--- Assets/Scripts/AI/AITestingCameraMovement.cs
+++ Assets/Scripts/AI/AITestingCameraMovement.cs
@@ -13,6 +13,16 @@ public class AITestingCameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey("w"))
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 20 * Time.deltaTime, gameObject.transform.position.z);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 20 * Time.deltaTime, gameObject.transform.position.z);
+        }
+
         if (Input.GetKey("a"))
         {
             gameObject.transform.position = new Vector3(gameObject.transform.position.x - 20 * Time.deltaTime, gameObject.transform.position.y, gameObject.transform.position.z);
