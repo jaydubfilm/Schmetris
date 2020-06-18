2593be3d45237443064ba87791d26d7739302e3e
Alex Bedard-Reid <alexbedardreid@gmail.com>
2020-06-18 17:46:38 -0400

    Combos Combos Combos
    [MOD] Set the transform value on CollidableBase to be public get
    [MOD] Added a data structure OrphanMoveData to handle information for moving orphaned pieces
    [MOD] Cleaned & improved the existing Combos code
    [MOD] Moved the PathToCore to the BotExtensions File
    [MOD] Made the PathAlgorithm hidden as I want the simple HasPathToCore to be outfacing

--- Assets/Scripts/Utilities/Extensions/BotExtensions.cs
+++ Assets/Scripts/Utilities/Extensions/BotExtensions.cs
@@ -116,6 +116,91 @@ namespace StarSalvager.Utilities.Extensions
         #endregion //Obtaining Bits
         
         //============================================================================================================//
+        
+        #region Path to Core Checks
+        
+        /// <summary>
+        /// Returns whether or not this AttachableBase has a clear path to the core.
+        /// </summary>
+        /// <param name="bot"></param>
+        /// <param name="checking"></param>
+        /// <param name="toIgnore"></param>
+        /// <returns></returns>
+        public static bool HasPathToCore(this Bot bot, AttachableBase checking, List<Vector2Int> toIgnore = null)
+        {
+            var travelled = new List<Vector2Int>();
+            //Debug.LogError("STARTED TO CHECK HERE");
+            return bot.PathAlgorithm(checking, toIgnore, ref travelled);
+        }
+        
+        private static bool PathAlgorithm(this Bot bot, AttachableBase current, ICollection<Vector2Int> toIgnore, ref List<Vector2Int> travelled)
+        {
+            //If we're on (0, 0) we've reached the core, so go back up through 
+            if (current.Coordinate == Vector2Int.zero)
+                return true;
+
+            //Get list of attachables around the current attachable
+            var attachablesAround = bot.GetAttachablesAround<AttachableBase>(current);
+            
+            for (var i = 0; i < attachablesAround.Count; i++)
+            {
+                //If there's no attachable, keep going
+                if (attachablesAround[i] == null)
+                    continue;
+
+                // If ignore list contains this Coordinate, keep going
+                if (toIgnore.Contains(attachablesAround[i].Coordinate))
+                {
+                    //Debug.LogError($"toIgnore contains {around[i].Coordinate}");
+                    attachablesAround[i] = null;
+                    continue;
+                }
+
+                // If we've not already been at this Coordinate, keep going
+                if (!travelled.Contains(attachablesAround[i].Coordinate))
+                    continue;
+
+                //Debug.LogError($"travelled already contains {around[i].Coordinate}");
+                attachablesAround[i] = null;
+            }
+
+            //Check to see if the list is completely null
+            if (attachablesAround.All(ab => ab == null))
+            {
+                //Debug.LogError($"FAILED. Nothing around {current}", current);
+                return false;
+            }
+
+            //If everything checks out, lets say we've been here
+            travelled.Add(current.Coordinate);
+
+            //Get a list of all non-null Attachables ordered by the shortest distance to the core
+            var closestAttachables = attachablesAround.Where(ab => ab != null)
+                .OrderBy(ab => Vector2Int.Distance(Vector2Int.zero, ab.Coordinate));
+            
+            
+            var result = false;
+            //Go through all of the attachables (Closest to Furthest) until we run out
+            foreach (var attachableBase in closestAttachables)
+            {
+                result = bot.PathAlgorithm(attachableBase, toIgnore, ref travelled);
+
+                //Debug.LogError($"{result} when checking {current.gameObject.name} to {attachableBase.gameObject.name}");
+
+                //If something reached the core, just stop looping and let the system know
+                if (result)
+                    break;
+            }
+
+            //Debug.LogError($"Failed Totally at {current.Coordinate}", current);
+            return result;
+        }
+        
+        
+        #endregion //Path to Core Checks
+        
+        //============================================================================================================//
+
     }
 }
 
