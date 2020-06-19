fa0e87e1abd36191899957568b5831d6344a2b99
Alex Shulman <shulmanbusiness@gmail.com>
2020-06-19 17:06:12 -0400

    add new classes to namespace

--- Assets/Scripts/AI/EnemyManager.cs
+++ Assets/Scripts/AI/EnemyManager.cs
@@ -2,21 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using StarSalvager.Constants;
+using StarSalvager.Factories;
 using StarSalvager.AI;
 
-public class EnemyManager : MonoBehaviour
+namespace StarSalvager
 {
-    private Enemy[] m_enemies;
-
-    // Start is called before the first frame update
-    void Start()
+    public class EnemyManager : MonoBehaviour
     {
-        m_enemies = new Enemy[Values.numberEnemiesSpawn];
-    }
+        private Enemy[] m_enemies;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Start is called before the first frame update
+        void Start()
+        {
+            m_enemies = new Enemy[Values.numberEnemiesSpawn];
+
+            for (int i = 0; i < m_enemies.Length; i++)
+            {
+                Enemy newEnemy = FactoryManager.Instance.GetFactory<EnemyFactory>().CreateObject<Enemy>(ENEMY_TYPE.Enemy1);
+                m_enemies[i] = newEnemy;
+                m_enemies[i].transform.position = LevelManager.Instance.WorldGrid.GetCenterOfGridSquareInGridPosition(Values.gridSizeX / 2, Values.gridSizeY / 2);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
--- Assets/Scripts/AI/ObstacleManager.cs
+++ Assets/Scripts/AI/ObstacleManager.cs
@@ -1,42 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using StarSalvager.Factories;
 using StarSalvager.Constants;
 using StarSalvager.AI;
 using StarSalvager;
 using Unity.Jobs;
 using UnityEngine.Jobs;
 
-public class ObstacleManager : MonoBehaviour
+namespace StarSalvager
 {
-    private Bit[] m_bits;
-
-    //Variables used for job scheduling system
-    PositionUpdateJob m_positionUpdateJob;
-    TransformAccessArray m_obstacleTransformAccessArray;
-
-    // Start is called before the first frame update
-    void Start()
+    public class ObstacleManager : MonoBehaviour
     {
-        m_bits = new Bit[Values.numberBitsSpawn];
-        Transform[] transformArray = new Transform[m_bits.Length];
+        private Bit[] m_bits;
 
-        for (int i = 0; i < m_bits.Length; i++)
+        //Variables used for job scheduling system
+        PositionUpdateJob m_positionUpdateJob;
+        TransformAccessArray m_obstacleTransformAccessArray;
+
+        struct PositionUpdateJob : IJobParallelForTransform
         {
-            Bit newBit = GameObject.Instantiate(LevelManager.Instance.BitTestPrefab);
-            m_bits[i] = newBit;
-            Vector2 position = LevelManager.Instance.WorldGrid.GetRandomGridSquareWorldPosition();
-            m_bits[i].transform.position = position;
-            transformArray[i] = m_bits[i].transform;
-            LevelManager.Instance.WorldGrid.SetObstacleInGridSquare(position, true);
+            public Vector3 distanceToMove;
+
+            public void Execute(int i, TransformAccess transform)
+            {
+                transform.position -= distanceToMove;
+            }
         }
 
-        m_obstacleTransformAccessArray = new TransformAccessArray(transformArray);
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
+            m_bits = new Bit[Values.numberBitsSpawn];
+            Transform[] transformArray = new Transform[m_bits.Length];
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            for (int i = 0; i < m_bits.Length; i++)
+            {
+                Bit newBit = GameObject.Instantiate(LevelManager.Instance.BitTestPrefab);
+                m_bits[i] = newBit;
+                Vector2 position = LevelManager.Instance.WorldGrid.GetRandomGridSquareWorldPosition();
+                m_bits[i].transform.position = position;
+                transformArray[i] = m_bits[i].transform;
+                LevelManager.Instance.WorldGrid.SetObstacleInGridSquare(position, true);
+            }
+
+            m_obstacleTransformAccessArray = new TransformAccessArray(transformArray);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
