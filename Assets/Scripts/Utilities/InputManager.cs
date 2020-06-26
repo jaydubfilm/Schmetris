1f386088b332713e21b621252bfa78819ca72a5b
Alex Shulman <shulmanbusiness@gmail.com>
2020-06-26 17:05:37 -0400

    Beginnings of DAS timer

--- Assets/Scripts/Utilities/InputManager.cs
+++ Assets/Scripts/Utilities/InputManager.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine.InputSystem;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace StarSalvager.Utilities.Inputs
 {
@@ -53,12 +55,15 @@ namespace StarSalvager.Utilities.Inputs
             Input.Actions.Default.Rotate.performed -= Rotate;
         }
 
+        float _prevMove = 0.0f;
+
         private void SideMovement(InputAction.CallbackContext ctx)
         {
             var move = ctx.ReadValue<float>();
+            _prevMove = move;
 
             var noObstacles = _obstacleManager is null;
-            
+
             foreach (var bot in _bots)
             {
                 bot.Move(move, noObstacles);
@@ -72,6 +77,39 @@ namespace StarSalvager.Utilities.Inputs
             _enemyManager.Move(move);
             _cameraController.Move(move);
             LevelManager.Instance.ProjectileManager.Move(move);
+
+            StartCoroutine(dasTimer(move));
+        }
+
+        private void SideMovement(float move)
+        {
+            var noObstacles = _obstacleManager is null;
+
+            foreach (var bot in _bots)
+            {
+                bot.Move(move, noObstacles);
+            }
+
+
+            if (noObstacles)
+                return;
+
+            _obstacleManager.Move(move);
+            _enemyManager.Move(move);
+            _cameraController.Move(move);
+
+            LevelManager.Instance.ProjectileManager.Move(move);
+        }
+
+        IEnumerator dasTimer(float direction)
+        {
+            SideMovement(0.0f);
+            yield return new WaitForSeconds(0.2f);
+
+            if (_prevMove == direction)
+            {
+                SideMovement(direction);
+            }
         }
 
         private void Rotate(InputAction.CallbackContext ctx)
