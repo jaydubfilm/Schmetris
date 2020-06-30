c02c10797699632a2edecb853d86693fe4552671
Alex Shulman <shulmanbusiness@gmail.com>
2020-06-30 17:38:36 -0400

    Scene Loader system

---
+++ Assets/Scripts/Utilities/SceneLoader/SceneRoot.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace StarSalvager.SceneLoader
+{
+    public class SceneRoot : MonoBehaviour
+    {
+        public Scene Scene;
+        public bool IsStarted { get; private set; } = false;
+        public bool IsReady { get; private set; } = false;
+
+        protected virtual void Start()
+        {
+            Scene = gameObject.scene;
+            SceneLoader.SubscribeSceneRoot(this, Scene.name);
+
+            IsStarted = true;
+        }
+
+        public virtual void SetupScene()
+        {
+            IsReady = true;
+        }
+    }
+}
