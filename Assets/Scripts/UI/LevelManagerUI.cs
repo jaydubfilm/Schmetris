7a1d27ea90874db34579d4dbd19ae786708fc9b8
Alex Shulman <shulmanbusiness@gmail.com>
2020-07-10 14:03:19 -0400

    fix to pause system

--- Assets/Scripts/UI/LevelManagerUI.cs
+++ Assets/Scripts/UI/LevelManagerUI.cs
@@ -59,7 +59,6 @@ namespace StarSalvager.UI
 
             scrapyardButton.onClick.AddListener(() =>
             {
-                GameTimer.SetPaused(false);
                 m_levelManager.ProcessScrapyardUsageBeginAnalytics();
                 StarSalvager.SceneLoader.SceneLoader.ActivateScene("ScrapyardScene", "AlexShulmanTestScene");
             });
@@ -67,14 +66,12 @@ namespace StarSalvager.UI
             toScrapyardButton.onClick.AddListener(() =>
             {
                 m_levelManager.SavePlayerData();
-                GameTimer.SetPaused(false);
                 m_levelManager.ProcessScrapyardUsageBeginAnalytics();
                 StarSalvager.SceneLoader.SceneLoader.ActivateScene("ScrapyardScene", "AlexShulmanTestScene");
             });
 
             toMainMenuButton.onClick.AddListener(() =>
             {
-                GameTimer.SetPaused(false);
                 StarSalvager.SceneLoader.SceneLoader.ActivateScene("MainMenuScene", "AlexShulmanTestScene");
             });
 
