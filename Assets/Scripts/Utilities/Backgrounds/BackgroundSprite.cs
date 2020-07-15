87a189251d9b0c403ee910485c38f7955493e011
Alex Bedard-Reid <alexbedardreid@gmail.com>
2020-07-15 13:57:48 -0400

    Background Finishing Touches

--- Assets/Scripts/Utilities/Backgrounds/BackgroundSprite.cs
+++ Assets/Scripts/Utilities/Backgrounds/BackgroundSprite.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using StarSalvager.Cameras.Data;
+using StarSalvager.Utilities.Extensions;
+using StarSalvager.Values;
 using UnityEngine;
 
 namespace StarSalvager.Utilities.Backgrounds
@@ -13,8 +15,8 @@ namespace StarSalvager.Utilities.Backgrounds
         [SerializeField]
         private float _zDepth;
 
-        [SerializeField]
-        private Vector2 startPosition;
+        //[SerializeField]
+        //private Vector2 startPosition;
 
         public bool parentIsCamera => _parentIsCamera;
         [SerializeField]
@@ -28,12 +30,17 @@ namespace StarSalvager.Utilities.Backgrounds
         private Vector2 moveSpeed;
 
         [SerializeField]
+        private float horizontalMoveSpeed;
+
+        [SerializeField]
         private Sprite[] spriteOptions;
         
         
         private new Transform transform;
         private new SpriteRenderer renderer;
         private IBackground _backgroundImplementation;
+        
+        private Plane[] planes;
 
         //============================================================================================================//
 
@@ -41,24 +48,59 @@ namespace StarSalvager.Utilities.Backgrounds
         {
             transform = gameObject.transform;
             renderer = GetComponent<SpriteRenderer>();
-            
-            transform.position = new Vector3(startPosition.x, startPosition.y, zDepth);
+
+            var pos = transform.position;
+            pos.z = zDepth;
+            transform.position = pos;
             
             if(parentIsCamera)
                 transform.SetParent(cameraTransform, true);
 
             //TODO Maybe i can set the color random as well?
             renderer.sprite = spriteOptions[Random.Range(0, spriteOptions.Length)];
+
+            if (moveSpeed == Vector2.zero)
+            {
+                enabled = false;
+                return;
+            }
             
+            planes = GeometryUtility.CalculateFrustumPlanes(Camera.main);
+
         }
 
+        [SerializeField, ReadOnly]
+        private Vector3 horizontalMove;
+        [SerializeField, ReadOnly]
+        private float movingDirection;
+        [SerializeField, ReadOnly]
+        private DIRECTION direction;
+        
         public void UpdatePosition()
         {
-            var pos = (Vector2)transform.position;
-            pos += moveSpeed * Time.deltaTime;
+            direction = Globals.MovingDirection;
+            
+            movingDirection = Globals.MovingDirection.GetHorizontalDirectionFloat();
+            
+            horizontalMove = Vector3.right *
+                                 (horizontalMoveSpeed * Globals.MovingDirection.GetHorizontalDirectionFloat());
+            
+            var pos = transform.position;
+            pos += ((Vector3)moveSpeed +horizontalMove) * Time.deltaTime;
 
             transform.position = pos;
+
+
+            if (!GeometryUtility.TestPlanesAABB(planes, renderer.bounds) && transform.position.y < 0f)
+            {
+                pos = transform.localPosition;
+                pos.y = Mathf.Abs(pos.y * 1.2f);
+                
+                transform.localPosition = pos;
+            }
             
+            
+
             //TODO Need to check if this object is in the camera view
         }
 
