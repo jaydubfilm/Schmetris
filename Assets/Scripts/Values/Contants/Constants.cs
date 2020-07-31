49b1f414cfaac113542abdd973f1d0ad570741ed
Alex Shulman <shulmanbusiness@gmail.com>
2020-07-31 12:52:06 -0400

    new enemy profiles

--- Assets/Scripts/Values/Contants/Constants.cs
+++ Assets/Scripts/Values/Contants/Constants.cs
@@ -8,12 +8,12 @@ namespace StarSalvager.Values
     {
         public const float gridCellSize = 1.28f;
         private const float gridWidthRelativeToScreen = 3.0f;
-        private const float gridHeightRelativeToScreen = 2.0f;
+        private const float gridHeightRelativeToScreen = 1.25f;
         public const float timeForAsteroidsToFall = 0.25f;
         public const int enemyGridScanRadius = 3;
         public const float obstacleMass = 2.0f;
         public const float botHorizontalSpeed = 30.0f;
-        public const int initialColumnsOnScreen = 31;
+        public const int initialColumnsOnScreen = 51;
 
         
 
