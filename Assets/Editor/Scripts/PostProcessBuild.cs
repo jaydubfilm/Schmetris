0c256bc30f362445b28fa68ae4da7a15e9b0f629
Alex Bedard-Reid <alexbedardreid@gmail.com>
2020-07-31 15:47:50 -0400

    Fixing directory generation post build

---
+++ Assets/Editor/Scripts/PostProcessBuild.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+namespace StarSalvager.Editor
+{
+    public class PostProcessBuild
+    {
+        private static string EDITOR_PATH = Application.dataPath + "/RemoteData/AddToBuild/";
+        private static string BUILD_PATH = "/RemoteData/AddToBuild/";
+        
+        [PostProcessBuild(1)]
+        public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+        {
+            Debug.Log($"Moving files from {EDITOR_PATH} to Build path");
+            
+            var editorDirectory = new DirectoryInfo(EDITOR_PATH);
+            var files = editorDirectory.GetFiles("*.txt");
+            
+
+            var buildDirectory = new DirectoryInfo(pathToBuiltProject).Parent;
+            
+            if (!Directory.Exists(Path.Combine(buildDirectory.FullName, BUILD_PATH)))
+                buildDirectory = Directory.CreateDirectory(buildDirectory.FullName + BUILD_PATH);
+
+            foreach (var file in files)
+            {
+                var path = Path.Combine(buildDirectory.FullName, file.Name);
+                file.CopyTo(path);
+                
+                Debug.Log($"Copied {file.Name} to {path}");
+            }
+        }
+    }
+}
+
