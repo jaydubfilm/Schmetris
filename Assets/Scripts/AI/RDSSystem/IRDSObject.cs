f527837d7fc3c7cbc75a95e3e4a8db148d1600f6
Alex Shulman <shulmanbusiness@gmail.com>
2020-08-06 10:13:05 -0400

    RDS system prep work

---
+++ Assets/Scripts/AI/RDSSystem/IRDSObject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StarSalvager
+{
+    public interface IRDSObject
+    {
+        double rdsProbability { get; set; } // The chance for this item to drop
+        bool rdsUnique { get; set; }        // Only drops once per query
+        bool rdsAlways { get; set; }        // Drops always
+        bool rdsEnabled { get; set; }       // Can it drop now?
+
+        /// <summary>
+        /// Occurs before all the probabilities of all items of the current RDSTable
+        /// are summed up together.
+        /// This is the moment to modify any settings immediately before a result is calculated.
+        /// </summary>
+        event EventHandler rdsPreResultEvaluation;
+        /// <summary>
+        /// Occurs when this RDSObject has been hit by the Result procedure.
+        /// (This means, this object will be part of the result set).
+        /// </summary>
+        event EventHandler rdsHit;
+        /// <summary>
+        /// Occurs after the result has been calculated and the result set is complete, but before
+        /// the RDSTable's Result method exits.
+        /// </summary>
+        //event ResultEventHandler rdsPostResultEvaluation;
+
+        void OnRDSPreResultEvaluation(EventArgs e);
+        void OnRDSHit(EventArgs e);
+        //void OnRDSPostResultEvaluation(ResultEventArgs e);
+    }
+}
---
+++ Assets/Scripts/AI/RDSSystem/IRDSTable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StarSalvager
+{
+    public interface IRDSTable : IRDSObject
+    {
+        int rdsCount { get; set; }       // How many items shall drop from this table?
+        IEnumerable<IRDSObject> rdsContents { get; } // The contents of the table
+        IEnumerable<IRDSObject> rdsResult { get; }   // The Result set
+    }
+}   
