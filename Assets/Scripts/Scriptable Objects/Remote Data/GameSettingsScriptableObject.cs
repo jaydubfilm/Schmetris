21d464feb0b5b4b9afe1d7d518188e0d9fd6ae14
Bradykin <shulmanbusiness@gmail.com>
2020-09-02 14:19:12 -0400

    tweak game settings

--- Assets/Scripts/Scriptable Objects/Remote Data/GameSettingsScriptableObject.cs
+++ Assets/Scripts/Scriptable Objects/Remote Data/GameSettingsScriptableObject.cs
@@ -1,4 +1,5 @@
-﻿using StarSalvager.Values;
+﻿using Sirenix.OdinInspector;
+using StarSalvager.Values;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,7 +20,9 @@ namespace StarSalvager.ScriptableObjects
         public float missionReminderFrequency = 25.0f;
 
         public bool cameraUseInputMotion = true;
+        [ShowIf("cameraUseInputMotion", true)]
         public float cameraSmoothing = 8.0f;
+        [ShowIf("cameraUseInputMotion", true)]
         public float cameraOffsetBounds = 0.5f;
 
         public void SetupGameSettings()
