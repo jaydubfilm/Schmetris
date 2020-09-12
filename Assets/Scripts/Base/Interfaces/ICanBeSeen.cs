bd67e902a8146c4cf8f841e8d551d097abbe5a59
Alex Bedard-Reid <alexbedardreid@gmail.com>
2020-09-12 16:45:02 -0400

    Added Missing scripts

---
+++ Assets/Scripts/Base/Interfaces/ICanBeSeen.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace StarSalvager
+{
+    public interface ICanBeSeen
+    {
+        bool IsSeen { get; set; }
+        float CameraCheckArea { get; }
+
+        Transform transform { get; }
+
+        void RegisterCanBeSeen();
+        void UnregisterCanBeSeen();
+
+        void EnteredCamera();
+        void ExitedCamera();
+    }
+
+}
