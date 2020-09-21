37aa05ac4fcaf6d0b31b79fba4b1f7b722d75071
Bradykin <shulmanbusiness@gmail.com>
2020-09-20 20:27:32 -0400

    fix being able to move bot while dead

--- Assets/Scripts/Utilities/Input/InputManager.cs
+++ Assets/Scripts/Utilities/Input/InputManager.cs
@@ -326,7 +326,10 @@ namespace StarSalvager.Utilities.Inputs
         {
             if (_moveOnInput == null)
                 return;
-            
+
+            if (LevelManager.Instance.BotDead)
+                return;
+
             for (var i = _moveOnInput.Count - 1; i >= 0; i--)
             {
                 var move = _moveOnInput[i];
@@ -348,6 +351,9 @@ namespace StarSalvager.Utilities.Inputs
                 return;
             
             if (isPaused)
+                return;
+
+            if (LevelManager.Instance.BotDead)
                 return;
 
             var rot = ctx.ReadValue<float>();
@@ -391,6 +397,11 @@ namespace StarSalvager.Utilities.Inputs
             
             if(ctx.ReadValue<float>() == 1f)
                 GameTimer.SetPaused(!isPaused);
+        }
+
+        public void CancelMove()
+        {
+            Move(0);
         }
         
         #endregion //Inputs
