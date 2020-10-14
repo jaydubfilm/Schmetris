947a11a894f423d7dfd4c95547a0fe4bd38b9abb
Alex Bedard-Reid <alexbedardreid@gmail.com>
2020-10-14 11:44:32 -0400

    Tutorial Manager & New PART_TYPE values

--- Assets/Scripts/Tutorial/TutorialManager.cs
+++ Assets/Scripts/Tutorial/TutorialManager.cs
@@ -1,29 +1,112 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 using Input = StarSalvager.Utilities.Inputs.Input;
 
 namespace StarSalvager.Tutorial
 {
     public class TutorialManager : MonoBehaviour, IInput
     {
-        //sealed 
+        [Serializable]
+        public struct TutorialStepData
+        {
+            [FoldoutGroup("$title")]
+            public string title;
+            [HorizontalGroup("$title/UseWait"), ToggleLeft, LabelWidth(50f)]
+            public bool useWaitTime;
+            [HorizontalGroup("$title/UseWait"), EnableIf("useWaitTime"), HideLabel, SuffixLabel("Seconds", true)]
+            public float waitTime;
+            [TextArea,FoldoutGroup("$title")]
+            public string text;
+        }
+
+        [SerializeField]
+        private List<TutorialStepData> tutorialStepDatas;
+        
+
+        [SerializeField]
+        private TMP_Text text;
+        [SerializeField]
+        private Slider slider;
+        [SerializeField]
+        private Image image;
+        
         private bool _readyForInput;
         private bool _keyPressed;
+p
+        private List<IEnumerator> _tutorialStepCoroutines;
 
         //Unity Functions
         //====================================================================================================================//
         
         // Start is called before the first frame update
-        void Start()
+        private void Start()
         {
+            InitInput();
 
+            SetupTutorial();
+        }
+
+        private void OnDisable()
+        {
+            DeInitInput();
         }
 
         //Tutorial Functions
         //====================================================================================================================//
+
+        private void SetupTutorial()
+        {
+            _tutorialStepCoroutines = new List<IEnumerator>
+            {
+                //Step1Coroutine(),
+                //Step2Coroutine(),
+                //Step3Coroutine(),
+            };
+            
+            StartCoroutine(MainTutorialCoroutine());
+
+        }
+
+        private IEnumerator MainTutorialCoroutine()
+        {
+            foreach (var stepCoroutine in _tutorialStepCoroutines)
+            {
+                yield return StartCoroutine(stepCoroutine);
+            }
+        }
+
+        //Generic Tutorial Steps
+        //====================================================================================================================//
+
+        private IEnumerator WaitAnyKeyStep(TutorialStepData tutorialStepData)
+        {
+            text.text = tutorialStepData.text;
+            yield return new WaitForAnyKey(this);
+        }
+        private IEnumerator WaitTimeSliderStep(TutorialStepData tutorialStepData)
+        {
+            text.text = tutorialStepData.text;
+            
+            yield return new WaitTimeSlider(tutorialStepData.waitTime, slider);
+            
+            yield return new WaitForAnyKey(this);
+        }
         
-        
+        private IEnumerator WaitTimeImageStep(TutorialStepData tutorialStepData)
+        {
+            text.text = tutorialStepData.text;
+            
+            yield return new WaitTimeImage(tutorialStepData.waitTime, image);
+            
+            yield return new WaitForAnyKey(this);
+        }
 
         //IInput Functions
         //====================================================================================================================//
@@ -38,8 +121,8 @@ namespace StarSalvager.Tutorial
 
         public void DeInitInput()
         {
-            Input.Actions.Default.Any.Enable();
-            Input.Actions.Default.Any.performed += AnyKeyPressed;
+            Input.Actions.Default.Any.Disable();
+            Input.Actions.Default.Any.performed -= AnyKeyPressed;
         }
 
         //Input Functions
@@ -47,31 +130,95 @@ namespace StarSalvager.Tutorial
 
         private void AnyKeyPressed(InputAction.CallbackContext ctx)
         {
-            _keyPressed = true;
+            var pressed = ctx.ReadValue<float>() == 1f;
+            
+            _keyPressed = pressed;
         }
 
         //Utility Coroutines
         //====================================================================================================================//
         
-        private IEnumerator WaitAnyKey()
+        private sealed class WaitForAnyKey : IEnumerator
         {
-            yield return new WaitUntil(() => _keyPressed);
-
-            _keyPressed = false;
-        }
-
-        private IEnumerator WaitTimeCoroutine(float seconds)
-        {
-            var t = 0f;
-
-            while (t < seconds)
+            private readonly TutorialManager _tutorialManager;
+            
+            public bool MoveNext()
             {
-                t += Time.deltaTime;
-
-                //TODO Update some UI Slider
+                if (!_tutorialManager._keyPressed) 
+                    return true;
                 
-                yield return null;
+                _tutorialManager._keyPressed = false;
+                Debug.Log("Any Key Pressed");
+                return false;
+            }
+
+            public void Reset() { }
+
+            public object Current => null;
+
+            public WaitForAnyKey(TutorialManager tutorialManager)
+            {
+                Debug.Log("Waiting for Any Key");
+                _tutorialManager = tutorialManager;
             }
         }
+        private sealed class WaitTimeSlider : IEnumerator
+        {
+            private readonly Slider _slider;
+            private readonly float _startTime;
+            private float _timePast;
+            
+            public bool MoveNext()
+            {
+                if (_timePast >= _startTime)
+                    return false;
+                
+                _timePast += Time.deltaTime;
+                _slider.value = _timePast / _startTime;
+                
+                return true;
+            }
+
+            public void Reset() { }
+
+            public object Current => null;
+
+            public WaitTimeSlider(float time, Slider slider)
+            {
+                _startTime = time;
+                _timePast = 0;
+                _slider = slider;
+            }
+        }
+        private sealed class WaitTimeImage : IEnumerator
+        {
+            private readonly Image _image;
+            private readonly float _startTime;
+            private float _timePast;
+            
+            public bool MoveNext()
+            {
+                if (_timePast >= _startTime)
+                    return false;
+                
+                _timePast += Time.deltaTime;
+                _image.fillAmount = _timePast / _startTime;
+                
+                return true;
+            }
+
+            public void Reset() { }
+
+            public object Current => null;
+
+            public WaitTimeImage(float time, Image image)
+            {
+                _startTime = time;
+                _timePast = 0;
+                _image = image;
+            }
+        }
+        //====================================================================================================================//
+        
     }
 }
