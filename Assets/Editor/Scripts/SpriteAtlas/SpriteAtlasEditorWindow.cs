1cd55f977382a9e7d4809ede6fb43dfa2181e752
Alex Bedard-Reid <alexbedardreid@gmail.com>
2020-10-17 14:47:35 -0400

    Sprite Atlas Editor

---
+++ Assets/Editor/Scripts/SpriteAtlas/SpriteAtlasEditorWindow.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using StarSalvager.Editor.CustomEditors;
+using StarSalvager.Factories;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace StarSalvager.Editor
+{
+    public class SpriteAtlasEditorWindow : OdinEditorWindow
+    {
+        [Serializable]
+        public struct PartAtlasData
+        {
+            public PART_TYPE type;
+            public string name;
+
+            [ValueDropdown("GetFolders"), OnValueChanged("SetSprites")]
+            public string selectedVersion;
+
+            [ReadOnly, HideIf("NoPath")] 
+            public Texture2D[] sprites;
+
+
+
+            public ValueDropdownList<string> GetFolders()
+            {
+                var folder = Path.Combine(_spriteAtlasSettings.partSpritePath, type.ToString());
+                var subDirectories = new DirectoryInfo(folder).GetDirectories();
+
+                var valueDropdownList = new ValueDropdownList<string>();
+
+                foreach (var subDirectory in subDirectories)
+                {
+                    valueDropdownList.Add(new ValueDropdownItem<string>
+                    {
+                        Text = subDirectory.Name,
+                        Value = subDirectory.FullName
+                    });
+                }
+                
+
+                return valueDropdownList;
+            }
+
+            private void SetSprites()
+            {
+                if (string.IsNullOrEmpty(selectedVersion))
+                    return;
+
+                var textures = new List<Texture2D>();
+                var files = new DirectoryInfo(selectedVersion).GetFiles("*.png");
+
+                foreach (var fileInfo in files)
+                {
+                    var path = fileInfo.FullName.Replace("\\", "/").Replace(Application.dataPath, "Assets");
+                    
+                    var texture =  (Texture2D)AssetDatabase.LoadAssetAtPath(path, typeof(Texture2D));
+                    
+                    if(texture is null)
+                        continue;
+                    
+                    textures.Add(texture);
+                }
+
+                sprites = textures.ToArray();
+            }
+
+            private bool NoPath()
+            {
+                return string.IsNullOrEmpty(selectedVersion);
+            }
+        }
+        
+        
+        private static SpriteAtlasSettingsScriptableObject _spriteAtlasSettings;
+        private static SpriteAtlasEditorWindow _window;
+        
+        [MenuItem("Window/Star Salvager/Sprite Atlas Editor")]
+        public static void BulkPartCostEditor()
+        {
+            _window = GetWindow<SpriteAtlasEditorWindow>("Sprite Atlas Editor", true);
+            _window.Show();
+
+            var settings = AssetDatabase.LoadAssetAtPath<SpriteAtlasSettingsScriptableObject>("Assets/Editor/Sprite Atlas Settings.asset");
+
+            _spriteAtlasSettings = settings
+                ? settings
+                : throw new NotImplementedException("Need to create the Settings asset if none is present");
+
+            _window.partAtlasDatas = _window.SetupPartList();
+
+            //_window._partCostDatas = ToPartCostDataList(FindObjectOfType<FactoryManager>().PartsRemoteData.partRemoteData);
+
+            /*if (Selection.activeObject != null && Selection.activeObject.GetType() == typeof(Texture2D))
+                spriteTools.spritesheet = (Texture2D) Selection.activeObject;*/
+        }
+
+        [SerializeField]
+        private List<PartAtlasData> partAtlasDatas;
+
+        private List<PartAtlasData> SetupPartList()
+        {
+            var outList = new List<PartAtlasData>();
+            
+
+
+            foreach (PART_TYPE part in Enum.GetValues(typeof(PART_TYPE)))
+            {
+                var path = Path.Combine(_spriteAtlasSettings.partSpritePath, part.ToString());
+                if (!Directory.Exists(path))
+                    continue;
+                
+                outList.Add(new PartAtlasData
+                {
+                    type = part,
+                    name = part.ToString()
+                });
+            }
+
+
+            return outList;
+        }
+    }
+}
---
+++ Assets/Editor/Scripts/SpriteAtlas/SpriteAtlasSettingsScriptableObject.cs
@@ -0,0 +1,20 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.U2D;
+
+namespace StarSalvager.Editor.CustomEditors
+{
+    [CreateAssetMenu(fileName = "Sprite Atlas Settings", menuName = "Star Salvager/Sprite Atlas/Settings Asset")]
+    public class SpriteAtlasSettingsScriptableObject : ScriptableObject
+    {
+        public SpriteAtlas partsAtlas;
+        public SpriteAtlas bitsAtlas;
+
+
+        [FolderPath(AbsolutePath = true, RequireExistingPath = true, ParentFolder = "Assets/Sprites/")]
+        public string partSpritePath;
+
+        [FolderPath(AbsolutePath = true, RequireExistingPath = true, ParentFolder = "Assets/Sprites/")]
+        public string bitSpritePath;
+    }
+}
