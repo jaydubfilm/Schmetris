a88465b8f9d9b530e7654c60662b81472e1a2b23
Alex Bedard-Reid <alexbedardreid@gmail.com>
2020-10-30 15:30:42 -0400

    Fixed ScrapyardBot not showing the Recovery Drone Sprite

--- Assets/Scripts/Scrapyard/ScrapyardBot.cs
+++ Assets/Scripts/Scrapyard/ScrapyardBot.cs
@@ -91,6 +91,8 @@ namespace StarSalvager
             {
                 if(attachable is Part part && part.Type == PART_TYPE.CORE && Globals.IsRecoveryBot)
                     FactoryManager.Instance.GetFactory<PartAttachableFactory>().SetOverrideSprite(part, PART_TYPE.RECOVERY);
+                else if(attachable is ScrapyardPart scrapyardPart && scrapyardPart.Type == PART_TYPE.CORE && Globals.IsRecoveryBot)
+                    FactoryManager.Instance.GetFactory<PartAttachableFactory>().SetOverrideSprite(scrapyardPart, PART_TYPE.RECOVERY);
                 
                 AttachNewBit(attachable.Coordinate, attachable);
             }
