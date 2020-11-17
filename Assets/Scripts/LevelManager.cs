63ab7d2b1c42b0f93407d8bd808f47cddde6e4da
Bradykin <shulmanbusiness@gmail.com>
2020-11-17 11:20:32 -0500

    Implement treasure icons and backup loot tables

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -752,31 +752,6 @@ namespace StarSalvager
             endWaveMessage = "Wave Complete!";
 
             Toast.AddToast(endWaveMessage);
-            if (!Globals.OnlyGetWaveLootOnce || !PlayerDataManager.CheckIfCompleted(progressionSector, Globals.CurrentWave))
-            {
-                /*CurrentWaveData.ConfigureLootTable();
-                List<IRDSObject> newWaveLoot = CurrentWaveData.rdsTable.rdsResult.ToList();
-                DropLoot(newWaveLoot, -ObstacleManager.WorldElementsRoot.transform.position + Vector3.up * (10 * Constants.gridCellSize), false);*/
-
-                SectorLootTableScriptableObject sectorLootTable = FactoryManager.Instance.SectorRemoteData[Globals.CurrentSector].sectorRemoteDataLootTablesScriptable.GetLootTableAtIndex(PlayerDataManager.NumTimesBeatNewWaveInSector[Globals.CurrentSector]);
-                if (sectorLootTable != null)
-                {
-                    List<LevelRingNode> childNodesAccessible = PlayerDataManager.GetLevelRingNodeTree().TryFindNode(PlayerDataManager.GetLevelRingNodeTree().ConvertSectorWaveToNodeIndex(Globals.CurrentSector, Globals.CurrentWave)).childNodes;
-                    if (childNodesAccessible.Count == 0 || UnityEngine.Random.Range(0.0f, 1.0f) <= 0.33f)
-                    {
-                        sectorLootTable.ConfigureLootTable();
-                        List<IRDSObject> newWaveLoot = sectorLootTable.rdsTable.rdsResult.ToList();
-                        DropLoot(newWaveLoot, -ObstacleManager.WorldElementsRoot.transform.position + Vector3.up * (10 * Constants.gridCellSize), false);
-                        PlayerDataManager.NumTimesBeatNewWaveInSector[Globals.CurrentSector]++;
-                    }
-                }
-            }
-
-            int curNodeIndex = PlayerDataManager.GetLevelRingNodeTree().ConvertSectorWaveToNodeIndex(Globals.CurrentSector, Globals.CurrentWave);
-            if (!PlayerDataManager.GetPlayerPreviouslyCompletedNodes().Contains(curNodeIndex))
-            {
-                PlayerDataManager.AddCompletedNode(curNodeIndex);
-            }
 
             EndWaveState = true;
             LevelManagerUI.OverrideText = string.Empty;
@@ -790,6 +765,32 @@ namespace StarSalvager
 
             Random.InitState(CurrentWaveData.WaveSeed);
             Debug.Log("SET SEED " + CurrentWaveData.WaveSeed);
+
+            if (!Globals.OnlyGetWaveLootOnce || !PlayerDataManager.CheckIfCompleted(progressionSector, Globals.CurrentWave))
+            {
+                /*CurrentWaveData.ConfigureLootTable();
+                List<IRDSObject> newWaveLoot = CurrentWaveData.rdsTable.rdsResult.ToList();
+                DropLoot(newWaveLoot, -ObstacleManager.WorldElementsRoot.transform.position + Vector3.up * (10 * Constants.gridCellSize), false);*/
+
+                SectorLootTableScriptableObject sectorLootTable = FactoryManager.Instance.SectorRemoteData[Globals.CurrentSector].sectorRemoteDataLootTablesScriptable.GetLootTableAtIndex(PlayerDataManager.NumTimesGottenLootTableInSector[Globals.CurrentSector]);
+                if (sectorLootTable != null)
+                {
+                    List<LevelRingNode> childNodesAccessible = PlayerDataManager.GetLevelRingNodeTree().TryFindNode(PlayerDataManager.GetLevelRingNodeTree().ConvertSectorWaveToNodeIndex(Globals.CurrentSector, Globals.CurrentWave)).childNodes;
+                    if (childNodesAccessible.Count == 0 || UnityEngine.Random.Range(0.0f, 1.0f) <= 0.33f)
+                    {
+                        sectorLootTable.ConfigureLootTable();
+                        List<IRDSObject> newWaveLoot = sectorLootTable.rdsTable.rdsResult.ToList();
+                        DropLoot(newWaveLoot, -ObstacleManager.WorldElementsRoot.transform.position + Vector3.up * (10 * Constants.gridCellSize), false);
+                        PlayerDataManager.NumTimesGottenLootTableInSector[Globals.CurrentSector]++;
+                    }
+                }
+            }
+
+            int curNodeIndex = PlayerDataManager.GetLevelRingNodeTree().ConvertSectorWaveToNodeIndex(Globals.CurrentSector, Globals.CurrentWave);
+            if (!PlayerDataManager.GetPlayerPreviouslyCompletedNodes().Contains(curNodeIndex))
+            {
+                PlayerDataManager.AddCompletedNode(curNodeIndex);
+            }
 
             if (Globals.IsRecoveryBot)
             {
@@ -928,6 +929,22 @@ namespace StarSalvager
                         
                         FloatingText.Create($"+{gears}", position, Color.white);
                         
+                        break;
+                    }
+                    case RDSValue<BlockData> rdsValueBlockData:
+                    {
+                        if (EndWaveState)
+                        {
+                            switch (rdsValueBlockData.rdsValue.ClassType)
+                            {
+                                case nameof(Component):
+                                    PlayerDataManager.AddComponent((COMPONENT_TYPE)rdsValueBlockData.rdsValue.Type, 1);
+                                    loot.RemoveAt(i);
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
                         break;
                     }
                 }
