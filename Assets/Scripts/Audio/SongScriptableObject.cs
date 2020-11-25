0c1fe2292bd2bbcf8893f9cca0290a8336f4d494
Alex Bedard-Reid <alexbedardreid@gmail.com>
2020-11-25 13:07:02 -0500

    Music Audio Implementation

--- Assets/Scripts/Audio/SongScriptableObject.cs
+++ Assets/Scripts/Audio/SongScriptableObject.cs
@@ -2,33 +2,113 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Sirenix.OdinInspector;
 using Sirenix.Utilities;
+using StarSalvager.Utilities.Extensions;
 using UnityEngine;
 using UnityEngine.Audio;
 
 
+#if UNITY_EDITOR
+
 using UnityEditor;
+
+#endif
 
 namespace StarSalvager.Audio.ScriptableObjects
 {
     [CreateAssetMenu(fileName = "Song_Asset", menuName = "Star Salvager/Audio/Song Asset")]
     public class SongScriptableObject : ScriptableObject
     {
-        public AudioMixer masterMixer;
-        //[ShowInInspector, PropertyOrder(-1000), HideLabel]
+        private const string VOLUME = "Volume";
+        
         public string TrackName => name;
+        
+        [OnValueChanged(nameof(CreateStems)), Required, PropertyOrder(-1000)]
+        public AudioMixer mixer;
 
         [Title("$TrackName"), ListDrawerSettings(Expanded = true)]
         public StemData[] stems;
+
+        [HideInInspector]
+        public bool setup;
+
+        //====================================================================================================================//
+        
+#if UNITY_EDITOR
+
+        [Button, EnableIf("setup"), 
+         InfoBox("Force Update is a destructive event, and will destroy all data below", "setup", InfoMessageType = InfoMessageType.Error), 
+         PropertyOrder(-900)]
+        private void ForceUpdate()
+        {
+            var response = EditorUtility.DisplayDialog("Force Update",
+                "Updating the data below will remove all of its data. Are you sure you want to continue?",
+                "Update", "Cancel");
+
+            if (!response)
+                return;
+            
+            if (!setup)
+                return;
+
+            setup = false;
+            CreateStems();
+        }
+        private void CreateStems()
+        {
+            //TODO Should show some sort of warning
+            if (setup)
+                return;
+            
+            var groups = mixer.FindMatchingGroups(string.Empty);
+
+            stems = new StemData[groups.Length - 1];
+
+            for (int i = 0; i < groups.Length; i++)
+            {
+                var audioMixerGroup = groups[i];
+                
+                if (audioMixerGroup.name.Equals("Master"))
+                    continue;
+                
+                
+                stems[i-1] = new StemData
+                {
+                    name = audioMixerGroup.name,
+                    MixerGroup = audioMixerGroup
+                };
+            }
+            
+            
+            EditorUtility.SetDirty(this);
+            AssetDatabase.SaveAssets();
+
+            setup = true;
+        }
+        
+#endif
 
     }
 
     [Serializable]
     public struct StemData
     {
-        [FoldoutGroup("$name")]
+        public enum FADE
+        {
+            IN,
+            OUT
+        }
+        
+        
+        public string VOLUME => $"{name}_Volume";
+        
+        [FoldoutGroup("$name"), DisplayAsString]
         public string name;
+
+        [FoldoutGroup("$name")]
+        public AudioMixerGroup MixerGroup;
         
         [FoldoutGroup("$name")]
         public AudioClip clip;
@@ -40,6 +120,62 @@ namespace StarSalvager.Audio.ScriptableObjects
         
         [FoldoutGroup("$name"), Range(0f, 1f)]
         public float maxLevel;
+
+        public float GetFadeTime(FADE fadeDirection)
+        {
+            switch (fadeDirection)
+            {
+                case FADE.IN:
+                    return fadeIn.startDelay + fadeIn.time;
+                case FADE.OUT:
+                    return fadeOut.startDelay + fadeOut.time;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(fadeDirection), fadeDirection, null);
+            }
+        }
+        public void SetVolume(float volume)
+        {
+            MixerGroup.audioMixer.SetVolume(VOLUME, Mathf.Lerp(0f, maxLevel, volume));
+        }
+        
+        public IEnumerator FadeStem(FADE fade)
+        {
+            FadeData fadeData;
+            switch (fade)
+            {
+                case FADE.IN:
+                    fadeData = fadeIn;
+                    break;
+                case FADE.OUT:
+                    fadeData = fadeOut;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(fade), fade, null);
+            }
+            
+            var level = maxLevel;
+            var fadeVolume = VOLUME;
+            
+            if(fadeData.startDelay > 0)
+                yield return new WaitForSecondsRealtime(fadeData.startDelay);
+
+            float t = 0;
+            float time = fadeData.time;
+            while (t / time >= 1)
+            {
+                var td = fadeData.curve.Evaluate(t / time);
+
+                var vol = Mathf.Lerp(0f, level, td);
+
+                MixerGroup.audioMixer.SetVolume(fadeVolume, vol);
+
+                t += Time.unscaledDeltaTime;
+                yield return null;
+            }
+
+        }
+
+        
     }
 
     [Serializable]
--- Assets/Scripts/Song.cs
+++ Assets/Scripts/Song.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 using Sirenix.OdinInspector;
 using StarSalvager.Audio.ScriptableObjects;
 using UnityEngine;
@@ -12,10 +12,70 @@ namespace StarSalvager.Audio
         [SerializeField, Required]
         private SongScriptableObject song;
 
+        private bool _fading;
+        
+        [Button]
+        public void SetFullVolume()
+        {
+            foreach (var stem in song.stems)
+            {
+                stem.SetVolume(1f);
+            }
+        }
+        [Button]
+        public void Mute()
+        {
+            foreach (var stem in song.stems)
+            {
+                stem.SetVolume(0f);
+            }
+        }
+
+        //====================================================================================================================//
+        public void FadeInTrack()
+        {
+            if (_fading)
+                return;
+
+            StartCoroutine(FadeTrack(StemData.FADE.IN));
+        }
+
+        public void FadeOutTrack()
+        {
+            if (_fading)
+                return;
+
+            StartCoroutine(FadeTrack(StemData.FADE.OUT));
+        }
+
+        //====================================================================================================================//
+        
+        private  IEnumerator FadeTrack(StemData.FADE fadeDirection)
+        {
+            _fading = true;
+            
+            var maxFadeTime = song.stems.Max(s => s.GetFadeTime(fadeDirection));
+            var stems = song.stems.Select(stem => stem.FadeStem(fadeDirection)).ToArray();
+            var coroutines = new Coroutine[stems.Length];
+
+            for (int i = 0; i < stems.Length; i++)
+            {
+                coroutines[i] = StartCoroutine(stems[i]);
+            }
+
+            yield return new WaitForSecondsRealtime(maxFadeTime);
+            _fading = false;
+        }
+
+        //====================================================================================================================//
+        
         [Button]
         private void SetupObject()
         {
             throw new NotImplementedException();
         }
+
+        //====================================================================================================================//
+        
     }
 }
