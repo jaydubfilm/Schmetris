76053d4979cef17f44a07722deb8c149b1bf0671
Alex Bedard-Reid <alexbedardreid@gmail.com>
2020-12-09 16:12:38 -0500

    Splash Screen

--- Assets/Scripts/Prototyping/UIImageMoveCollection.cs
+++ Assets/Scripts/Prototyping/UIImageMoveCollection.cs
@@ -11,14 +11,27 @@ namespace StarSalvager.Prototype
 {
     public class UIImageMoveCollection : MonoBehaviour
     {
-        [SerializeField] private Transform mainTransform;
+        //[SerializeField] private Transform mainTransform;
 
-        [SerializeField] private Transform[] affectedTransforms;
+        [SerializeField] private Transform[] rotationTransforms;
+        [SerializeField] private Transform[] wobbleTransforms;
+        
 
         [SerializeField, MinMaxSlider(0, 100, true)]
         private Vector2 speedRange;
+        
+        [SerializeField, MinMaxSlider(0, 100, true)]
+        private Vector2 wobbleRange;
+        
+        [SerializeField, MinMaxSlider(0, 10, true)]
+        private Vector2 wobbleSpeedRange;
+
+        [SerializeField]
+        private AnimationCurve wobbleCurve;
 
         private float[] _rotationSpeeds;
+        private float[] _wobbleRanges;
+        private float[] _wobbleSpeeds;
 
         private bool _ready;
 
@@ -29,6 +42,9 @@ namespace StarSalvager.Prototype
         private void Start()
         {
             SetupRotations();
+            SetupWobbles();
+            
+            _ready = true;
         }
 
         // Update is called once per frame
@@ -43,37 +59,14 @@ namespace StarSalvager.Prototype
 
         private void OnValidate()
         {
-            if (affectedTransforms.IsNullOrEmpty())
+            if (rotationTransforms.IsNullOrEmpty())
                 return;
 
-            foreach (var affectedTransform in affectedTransforms)
+            foreach (var affectedTransform in rotationTransforms)
             {
                 affectedTransform.gameObject.name = affectedTransform.GetComponent<Image>().sprite.name;
             }
         }
-        
-        /*[Button]
-        private void Fix()
-        {
-            affectedTransforms = new Transform[transform.childCount];
-            
-            for (int i = 0; i < transform.childCount; i++)
-            {
-                affectedTransforms[i] = transform.GetChild(i);
-            }            
-            
-
-            foreach (var affectedTransform in affectedTransforms)
-            {
-                var spriteRenderer = affectedTransform.GetComponent<SpriteRenderer>();
-                var sprite = spriteRenderer.sprite;
-                affectedTransform.GetComponent<Image>().sprite = sprite;
-
-                affectedTransform.gameObject.name = sprite.name;
-                
-                DestroyImmediate(spriteRenderer);
-            }
-        }*/
 
 #endif
 
@@ -82,35 +75,62 @@ namespace StarSalvager.Prototype
         [Button, DisableInEditorMode]
         private void SetupRotations()
         {
-            if (affectedTransforms.IsNullOrEmpty())
+            if (rotationTransforms.IsNullOrEmpty())
             {
                 throw new Exception("No transforms to setup");
             }
 
-            _rotationSpeeds = new float[affectedTransforms.Length];
+            _rotationSpeeds = new float[rotationTransforms.Length];
             for (int i = 0; i < _rotationSpeeds.Length; i++)
             {
                 _rotationSpeeds[i] = Random.Range(speedRange.x, speedRange.y) * (Random.value > 0.5f ? -1f : 1f);
             }
 
-            _ready = true;
+            
         }
+
+        private void SetupWobbles()
+        {
+            if (wobbleTransforms.IsNullOrEmpty())
+            {
+                throw new Exception("No transforms to setup");
+            }
+
+            _wobbleRanges = new float[wobbleTransforms.Length];
+            _wobbleSpeeds = new float[wobbleTransforms.Length];
+            for (int i = 0; i < _wobbleRanges.Length; i++)
+            {
+                _wobbleRanges[i] = Random.Range(wobbleRange.x, wobbleRange.y);
+                _wobbleSpeeds[i] = Random.Range(wobbleSpeedRange.x, wobbleSpeedRange.y);
+            }
+        }
+
+        //====================================================================================================================//
+
 
         private void MoveTransforms()
         {
             for (int i = 0; i < _rotationSpeeds.Length; i++)
             {
-                var transform = affectedTransforms[i];
-                var dirToMain = (mainTransform.position - transform.position).normalized;
-
-                transform.position -= dirToMain * (0.01f * Time.deltaTime);
-
-
-                var eulerAngles = affectedTransforms[i].eulerAngles;
+                var eulerAngles = rotationTransforms[i].eulerAngles;
 
                 eulerAngles += Vector3.forward * (_rotationSpeeds[i] * Time.deltaTime);
 
-                affectedTransforms[i].eulerAngles = eulerAngles;
+                rotationTransforms[i].eulerAngles = eulerAngles;
+            }
+
+            for (int i = 0; i < _wobbleRanges.Length; i++)
+            {
+                var eulerAngles = wobbleTransforms[i].eulerAngles;
+
+                var td = wobbleCurve.Evaluate(Mathf.PingPong(Time.time, _wobbleSpeeds[i]) / _wobbleSpeeds[i]);
+
+                eulerAngles.z = Mathf.Lerp(
+                    -_wobbleRanges[i],
+                    _wobbleRanges[i],
+                    td);
+
+                wobbleTransforms[i].eulerAngles = eulerAngles;
             }
         }
     }
