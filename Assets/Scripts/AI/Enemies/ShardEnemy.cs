90fd4530934fd408e060203cce0d6568328de8f1
Bradykin <shulmanbusiness@gmail.com>
2021-01-20 09:10:24 -0500

    work on shards

--- Assets/Scripts/AI/Enemies/ShardEnemy.cs
+++ Assets/Scripts/AI/Enemies/ShardEnemy.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using StarSalvager.Cameras;
+using StarSalvager.Values;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,17 +8,42 @@ namespace StarSalvager.AI
 {
     public class ShardEnemy : Enemy
     {
-        public override bool IsAttachable => true;
+        public override bool IsAttachable => false;
         public override bool IgnoreObstacleAvoidance => true;
         public override bool SpawnHorizontal => false;
+
+        private bool m_isAccelerating;
+        private float m_accelerationAmount = 0.0f;
 
         //============================================================================================================//
 
         #region Movement
 
+        public override void ProcessMovement(Vector2 playerLocation)
+        {
+            if (CameraController.IsPointInCameraRect(transform.position, 0.6f))
+            {
+                if (!m_isAccelerating)
+                {
+                    if (playerLocation.x - transform.position.x <= 1.0f)
+                    {
+                        m_isAccelerating = true;
+                    }
+                }
+            }
+
+            if (m_isAccelerating)
+            {
+                m_accelerationAmount = Mathf.Max(2.0f, m_accelerationAmount + 3 * Time.deltaTime);
+            }
+
+            Vector3 fallAmount = Vector3.up * ((Constants.gridCellSize * Time.deltaTime) / Globals.TimeForAsteroidToFallOneSquare) * (1.0f + m_accelerationAmount);
+            transform.position -= fallAmount;
+        }
+
         public override Vector2 GetMovementDirection(Vector2 playerLocation)
         {
-            return playerLocation - (Vector2)transform.position;
+            return Vector2.down;
         }
 
         #endregion
@@ -36,6 +63,14 @@ namespace StarSalvager.AI
         }
 
         #endregion
+
+        public override void CustomRecycle(params object[] args)
+        {
+            m_isAccelerating = false;
+            m_accelerationAmount = 0;
+            
+            base.CustomRecycle(args);
+        }
 
         //============================================================================================================//
     }
