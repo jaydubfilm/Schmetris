9536cd8c64a0d3c33457d49d382c98a81aaca1ac
Alex Bedard-Reid <alexbedardreid@gmail.com>
2021-01-29 10:14:52 -0500

    Patches UI Improvements & Functionality Fixes

---
+++ Assets/Scripts/Utilities/Extensions/PatchIEnumerableExtensions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using StarSalvager.Factories;
+using StarSalvager.Parts.Data;
+
+namespace StarSalvager.Utilities.Extensions
+{
+    public static class PatchIEnumerableExtensions
+    {
+        public static int GetPatchUpgradersSum(this IEnumerable<PatchData> patches)
+        {
+            return patches.Where(x => x.Type == (int) PATCH_TYPE.GRADE).Sum(x => x.Level + 1);
+        }
+        
+        public static float GetPatchMultiplier(this IEnumerable<PatchData> patches, in PATCH_TYPE patchType)
+        {
+            //Find out
+            var pType = (int) patchType;
+            var patchesOfType = patches.Where(x => x.Type == pType).ToList().AsReadOnly();
+            
+            if (patchesOfType.Count == 0)
+                return 1;
+
+            var remoteData = FactoryManager.Instance.PatchRemoteData;
+            
+            var total = 0f;
+            foreach (var patchData in patchesOfType)
+            {
+                var data = remoteData.GetRemoteData(patchType)
+                    .GetDataValue<float>(patchData.Level, PartProperties.KEYS.Multiplier);
+
+                total += data;
+            }
+
+            switch (patchType)
+            {
+                case PATCH_TYPE.DAMAGE:
+                case PATCH_TYPE.RANGE:
+                    return 1 + total;
+                case PATCH_TYPE.FIRE_RATE:
+                    return  1 - total;
+                
+                case PATCH_TYPE.EFFICIENCY:
+                case PATCH_TYPE.DURATION:
+                case PATCH_TYPE.CRITICAL:
+                case PATCH_TYPE.ELECTRIC:
+                case PATCH_TYPE.CORROSIVE:
+                case PATCH_TYPE.REINFORCED:
+                case PATCH_TYPE.SPECIALIST:
+                    throw new NotImplementedException($"{patchType} not yet implemented");
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(patchType), patchType, null);
+            }
+        }
+        public static IReadOnlyDictionary<PATCH_TYPE, float> GetPatchMultipliers(this IEnumerable<PatchData> patches, params PATCH_TYPE[] patchTypes)
+        {
+            var outData = new Dictionary<PATCH_TYPE, float>();
+            
+            var patchDatas = patches as PatchData[] ?? patches.ToArray();
+            foreach (var patchType in patchTypes)
+            {
+                if(outData.ContainsKey(patchType))
+                    continue;
+
+                var mult = patchDatas.GetPatchMultiplier(patchType);
+                outData.Add(patchType, mult);
+            }
+
+            return outData;
+        }
+    }
+}
