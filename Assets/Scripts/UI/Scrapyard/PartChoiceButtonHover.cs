eb50eb82d9ec43ecf27b53660da92b791f2dfb5d
Alex Bedard-Reid <alexbedardreid@gmail.com>
2021-03-25 09:56:43 -0700

    Bug Fixes
    [ADD] Addedd PartChoiceButtonHover script to handle displaying part Detail window
    [MOD] Added OnHover for part selection to see more details regarding Parts
    [MOD] Added text to Part Details window to say whether part is manual
    [MOD] Added explosion when being hit by the Laser Turret
    [MOD] Added an additional Effect when making combos
    [FIX] Fixed the targeting Vectors not being correct for Data Leech & Borrower
    [FIX] Fixed the Mine Dimensions not being correct
    [FIX] Fixed the core Regen ability using 1:1 cost/healing

---
+++ Assets/Scripts/UI/Scrapyard/PartChoiceButtonHover.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using StarSalvager.Utilities.JsonDataTypes;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace StarSalvager.UI.Scrapyard
+{
+    public class PartChoiceButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        private static DroneDesignUI _droneDesignUI;
+        private PART_TYPE _partType;
+        private PartData _partData;
+
+        private new RectTransform transform;
+
+        public void SetPartType(in PART_TYPE partType)
+        {
+            if (_droneDesignUI == null)
+                _droneDesignUI = FindObjectOfType<DroneDesignUI>();
+            
+            if(transform == null)
+                transform = gameObject.transform as RectTransform;
+
+            _partType = partType;
+            _partData = new PartData
+            {
+                Type = (int) _partType,
+                Coordinate = Vector2Int.zero,
+                Patches = new []
+                {
+                    new PatchData(),
+                    new PatchData()
+                }
+            };
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            _droneDesignUI.ShowPartDetails(true, _partData, transform);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            _droneDesignUI.ShowPartDetails(false, new PartData(), null);
+        }
+    }
+}
