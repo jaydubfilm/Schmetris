2fd31d807bf93e3840c9bfa5403bad0dd4d8b0db
Alex Bedard-Reid <alexbedardreid@gmail.com>
2021-03-26 13:18:08 -0700

    Procedural work

--- Assets/Scripts/Scriptable Objects/Procedural/RingProfileDataScriptableObject.cs
+++ Assets/Scripts/Scriptable Objects/Procedural/RingProfileDataScriptableObject.cs
@@ -1,11 +1,52 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace StarSalvager.ScriptableObjects.Procedural
 {
+    [CreateAssetMenu(fileName = "Ring Data", menuName = "Star Salvager/Scriptable Objects/Ring Data")]
     public class RingProfileDataScriptableObject : ScriptableObject
     {
+        [Serializable]
+        public struct WaveSpawnData
+        {
+            public WaveProfileDataScriptableObject.WAVE_TYPE WaveType;
+
+            [AssetList(CustomFilterMethod = "HasRigidbodyComponent")]
+            public List<WaveProfileDataScriptableObject> waves;
+
+            [MinMaxSlider(1,10, true)]
+            public Vector2Int count;
+            
+            private bool HasRigidbodyComponent(WaveProfileDataScriptableObject obj)
+            {
+                return obj.WaveType == WaveProfileDataScriptableObject.WAVE_TYPE.SURVIVAL;
+            }
+
+        }
+        
+        [MinMaxSlider(3, 10, true)]
+        public Vector2Int sectorsRange = Vector2Int.one;
+        [MinMaxSlider(1, 5, true)]
+        public Vector2Int nodesRange = Vector2Int.one;
+
+        [MinMaxSlider(1, 3, true), ReadOnly]
+        public Vector2Int pathsRange = Vector2Int.one;
+
+        [InlineEditor]
+        public List<WreckNodeDataScriptableObject> wrecks;
+        [InlineEditor]
+        public List<WaveProfileDataScriptableObject> waves;
+
+
+        [BoxGroup("Survival Waves")]
+        public WaveSpawnData test = new WaveSpawnData
+        {
+            WaveType = WaveProfileDataScriptableObject.WAVE_TYPE.SURVIVAL
+        };
+
 
     }
 }
--- Assets/Scripts/Scriptable Objects/Procedural/WaveProfileDataScriptableObject.cs
+++ Assets/Scripts/Scriptable Objects/Procedural/WaveProfileDataScriptableObject.cs
@@ -1,11 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace StarSalvager.ScriptableObjects.Procedural
 {
+    [CreateAssetMenu(fileName = "Wave Profile Data", menuName = "Star Salvager/Scriptable Objects/Wave Profile Data")]
     public class WaveProfileDataScriptableObject : BaseMapNodeScriptableObject
     {
+        public enum WAVE_TYPE
+        {
+            SURVIVAL,
+            DEFENCE,
+            BONUS,
+            MINI_BOSS
+        }
+
+        [EnumToggleButtons, LabelWidth(90)] public WAVE_TYPE WaveType;
+
+        [MinMaxSlider(10, 300), Tooltip("Time is in Seconds"), ShowIf("ShowTime")]
+        public Vector2Int waveTimeRange;
+
+        #if UNITY_EDITOR
+        
+        private bool ShowTime()
+        {
+            return WaveType == WAVE_TYPE.BONUS || WaveType == WAVE_TYPE.SURVIVAL;
+        }
+        
+        #endif
         
     }
 }
