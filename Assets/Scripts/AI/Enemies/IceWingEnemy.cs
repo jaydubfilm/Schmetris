cc884c0107789e8ce3915973b07048e794f049d4
Alex Bedard-Reid <alexbedardreid@gmail.com>
2021-03-30 15:02:42 -0700

    Icewing enemy Implementation

---
+++ Assets/Scripts/AI/Enemies/IceWingEnemy.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Recycling;
+using StarSalvager.Cameras;
+using StarSalvager.Utilities;
+using StarSalvager.Values;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace StarSalvager.AI
+{
+    public class IceWingEnemy : Enemy
+    {
+
+        public override bool IgnoreObstacleAvoidance => true;
+        public override bool SpawnAboveScreen => true;
+
+        //Properties
+        //====================================================================================================================//
+
+        [SerializeField]
+        private float freezeTime;
+        
+        [SerializeField, Range(1,10)]
+        private int attackPasses = 2;
+        [SerializeField, Range(1,10)]
+        private int attackSwoops = 2;
+
+        [SerializeField]
+        private float attackSwoopHeight;
+        
+        [SerializeField]
+        private float apexWaitTime;
+        
+        [SerializeField, Range(0f,10f)]
+        private float anticipationWaitTime = 3f;
+
+
+        [SerializeField]
+        private float checkDistance;
+        [SerializeField, Range(0f, 1f), Tooltip("1.0 is 100% accurate and 0.0 is anywhere in front")]
+        private float dotThreshold = 0.1f;
+
+        [SerializeField]
+        private LineRenderer lineRenderer;
+
+        //====================================================================================================================//
+        
+        private float _travelTime;
+        private bool _isLeftPos;
+
+        private int _attackPassCount;
+        private int _attackSwoopCount;
+        private float _waitTimer;
+
+        private float _t;
+        private Vector2 _targetLocation;
+        private Vector2 _startLocation;
+        
+        
+        private Vector2 _currentPosition;
+        private Vector2 _lastPosition;
+
+        //====================================================================================================================//
+        
+        public override void LateInit()
+        {
+
+            //--------------------------------------------------------------------------------------------------------//
+            
+            //https://www.calculator.net/right-triangle-calculator.html
+            float CalculateEndWidth(in float dotThreshold, in float castDistance)
+            {
+                var angleA = Mathf.Acos(dotThreshold) * Mathf.Rad2Deg;
+                var lengthC = castDistance / Mathf.Cos(angleA);
+                var lengthA = Mathf.Sqrt(Mathf.Pow(lengthC, 2) - Mathf.Pow(castDistance, 2));
+
+                return lengthA / 2f;
+            }
+
+            //--------------------------------------------------------------------------------------------------------//
+            
+            base.LateInit();
+
+            _attackPassCount = 0;
+            _attackSwoopCount = 0;
+            _waitTimer = 0f;
+
+            lineRenderer.startWidth = 0;
+            lineRenderer.endWidth = 1f;
+            lineRenderer.widthMultiplier = CalculateEndWidth(dotThreshold, checkDistance);
+            lineRenderer.enabled = false;
+
+            SetState(STATE.MOVE);
+        }
+
+        //====================================================================================================================//
+        
+        protected override void StateChanged(STATE newState)
+        {
+            switch (newState)
+            {
+                case STATE.NONE:
+                    break;
+                case STATE.MOVE:
+                    _isLeftPos = Random.value > 0.5f;
+                    //TODO Determine if the enemy is on the left or right side of the screen
+                    _targetLocation = GetNewPosition(GetPos(_isLeftPos));
+                    break;
+                case STATE.FLEE:
+                    break;
+                case STATE.ANTICIPATION:
+                    _waitTimer = anticipationWaitTime;
+                    _attackSwoopCount = 0;
+                    break;
+                case STATE.ATTACK:
+                    _waitTimer = apexWaitTime;
+                    _t = 0f;
+                    _startLocation = GetNewPosition(GetPos(_isLeftPos));
+                    _targetLocation = GetNewPosition(GetPos(!_isLeftPos));
+
+                    var dist = Vector2.Distance(_startLocation, _targetLocation);
+                    _travelTime = dist / m_enemyData.MovementSpeed;
+                    break;
+                case STATE.DEATH:
+                    Recycler.Recycle<IceWingEnemy>(this);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+            }
+        }
+
+        //State Updates
+        //====================================================================================================================//
+
+        #region State Updates
+
+        protected override void StateUpdate()
+        {
+            _lastPosition = _currentPosition;
+            _currentPosition = Position;
+
+            Debug.DrawRay(_currentPosition, 
+                (_currentPosition - _lastPosition).normalized,
+                Color.red);
+            
+            if (_waitTimer > 0f)
+            {
+                _waitTimer -= Time.deltaTime;
+                return;
+            }
+            
+            switch (currentState)
+            {
+                case STATE.NONE:
+                case STATE.DEATH:
+                    return;
+                case STATE.MOVE:
+                    MoveState();
+                    break;
+                case STATE.FLEE:
+                    FleeState();
+                    break;
+                case STATE.ANTICIPATION:
+                    AnticipationState();
+                    break;
+                case STATE.ATTACK:
+                    AttackState();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(currentState), currentState, null);
+            }
+        }
+
+        private void MoveState()
+        {
+            //TODO Move towards target position
+            var currentPosition = transform.position;
+
+            if (Vector2.Distance(currentPosition, _targetLocation) > 0.1f)
+            {
+                transform.position = Vector2.MoveTowards(currentPosition, _targetLocation, EnemyMovementSpeed * Time.deltaTime);
+                MostRecentMovementDirection = (transform.position - currentPosition).normalized;
+                return;
+            }
+
+            SetState(STATE.ATTACK);
+            
+        }
+
+        private void FleeState()
+        {
+            //TODO Fly away once passes are done
+            //TODO Move off screen
+            var currentPosition = transform.position;
+
+            currentPosition += Vector3.up * (EnemyMovementSpeed * Time.deltaTime);
+
+            transform.position = currentPosition;
+
+            if (CameraController.IsPointInCameraRect(currentPosition))
+                return;
+            
+            //TODO When no longer visible, recycle this
+            SetState(STATE.DEATH);
+        }
+
+        private void AnticipationState()
+        {
+            SetState(STATE.ATTACK);
+        }
+
+        private void AttackState()
+        {
+
+            //--------------------------------------------------------------------------------------------------------//
+            
+            void TryFindBits(in Vector2 checkDirection)
+            {
+                var currentPosition = Position;
+                var angle = Mathf.Acos(dotThreshold) * Mathf.Rad2Deg;
+                var rayDirections = new[]
+                {
+                    Quaternion.Euler(0, 0, angle) * checkDirection,
+                    Quaternion.Euler(0, 0, angle / 2) * checkDirection,
+                    Quaternion.Euler(0, 0, -angle / 2) * checkDirection,
+                    Quaternion.Euler(0, 0, -angle) * checkDirection
+                };
+
+                foreach (var dir in rayDirections)
+                {
+                    Debug.DrawRay(currentPosition, dir * checkDistance, Color.yellow);
+                    
+                    var hit = Physics2D.Raycast(currentPosition, dir);
+                    
+                    if(hit.transform == null)
+                        continue;
+                    
+                    var hitCollidable = hit.transform.gameObject.GetComponent<ICanBeHit>();
+
+                    switch (hitCollidable)
+                    {
+                        case Bot bot:
+                        {
+                            var closestAttachable = bot.GetClosestAttachable(hit.point);
+                            if (closestAttachable is Bit bit)
+                                bit.SetFrozen(freezeTime);
+                        }
+                            break;
+                        case Bit bit when bit.Frozen == false:
+                            bit.SetFrozen(freezeTime);
+                            break;
+                            
+                    }
+                }
+
+            }
+
+            //--------------------------------------------------------------------------------------------------------//
+            
+
+            var t = _t / _travelTime;
+            
+            if (t >= 1f)
+            {
+                _isLeftPos = !_isLeftPos;
+
+                if (++_attackSwoopCount >= attackSwoops)
+                {
+                    SetState(++_attackPassCount >= attackPasses ? STATE.FLEE : STATE.ANTICIPATION);
+                }
+                else
+                    SetState(STATE.ATTACK);
+                
+                
+                return;
+            }
+
+            var attacking = t >= 0.25f && t <= 0.55f;
+            lineRenderer.enabled = attacking;
+
+            if (attacking)
+            {
+                var currentPosition = Position;
+                var checkDirection = (_currentPosition - _lastPosition).normalized;
+                
+                #region Unity Editor
+
+#if UNITY_EDITOR
+                void DebugLines()
+                {
+                
+                    Debug.DrawRay(currentPosition, checkDirection * checkDistance, Color.cyan);
+
+                    var angle = Mathf.Acos(dotThreshold) * Mathf.Rad2Deg;
+                    var up = Quaternion.Euler(0, 0, angle ) * checkDirection;
+                    var down = Quaternion.Euler(0, 0, -angle) * checkDirection;
+
+                    Debug.DrawRay(currentPosition, up * checkDistance, Color.blue);
+                    Debug.DrawRay(currentPosition, down * checkDistance, Color.blue);
+                }
+
+                DebugLines();
+#endif
+                #endregion //Unity Editor
+
+                TryFindBits(checkDirection);
+                
+                lineRenderer.SetPositions(new []
+                {
+                    currentPosition,
+                    currentPosition + (Vector3)(checkDirection * checkDistance)
+                });
+                
+            }
+            
+            var newPosition = Mathfx.Hermite(_startLocation, _targetLocation, t);
+            newPosition.y = Mathfx.HermiteCubed(newPosition.y, attackSwoopHeight, t);
+
+            transform.position = newPosition;
+
+            _t += Time.deltaTime;
+        }
+
+        #endregion //State Updates
+
+        private static Vector2 GetNewPosition(in float xPos, in float yPos = 0.85f)
+        {
+            //Used to ensure the CameraVisibleRect is updated
+            CameraController.IsPointInCameraRect(Vector2.zero, Constants.VISIBLE_GAME_AREA);
+            
+            var cameraRect = CameraController.VisibleCameraRect;
+            var xBounds = new Vector2(cameraRect.xMin, cameraRect.xMax);
+            var yBounds = new Vector2(cameraRect.yMin, cameraRect.yMax);
+
+            return new Vector2
+            {
+                x = Mathf.Lerp(xBounds.x, xBounds.y, xPos),
+                y = Mathf.Lerp(yBounds.x, yBounds.y, yPos)
+            };
+        }
+
+        private static float GetPos(in bool isLeft)
+        {
+            return isLeft ? 0.05f : 0.95f;
+        }
+
+        //Enemy Functions
+        //====================================================================================================================//
+        
+
+        public override void UpdateEnemy(Vector2 playerLocation)
+        {
+            StateUpdate();
+        }
+
+        protected override Vector2 GetMovementDirection(Vector2 playerLocation)
+        {
+            return (_currentPosition - _lastPosition).normalized;
+        }
+
+        public override Type GetOverrideType()
+        {
+            return typeof(IceWingEnemy);
+        }
+    }
+}
