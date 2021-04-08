d9b591c9003e3e9f3c42e3fdd61c983c5e907dd8
Alex Bedard-Reid <alexbedardreid@gmail.com>
2021-04-08 07:22:26 -0700

    Procedural Setup

--- Assets/Scripts/Scriptable Objects/Procedural/StageProfileScriptableObject.cs
+++ Assets/Scripts/Scriptable Objects/Procedural/StageProfileScriptableObject.cs
@@ -1,10 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace StarSalvager.ScriptableObjects.Procedural
 {
+    [CreateAssetMenu(fileName = "Stage Profile Data", menuName = "Star Salvager/Scriptable Objects/Stage Profile Data")]
     public class StageProfileDataScriptableObject : ScriptableObject
     {
+        //Enums
+        //====================================================================================================================//
+        
+        public enum TYPE
+        {
+            ASTEROID,
+            BUMPER,
+            CLOUD
+        }
+
+        //Structs
+        //====================================================================================================================//
+        
+        //public 
+
+        //====================================================================================================================//
+        
+        [MinMaxSlider(0,500)]
+        public Vector2Int bitSpawnsPerMin;
+
+        //public 
     }
 }
