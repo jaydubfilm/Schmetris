a77165310712dc5be0398aef5fb0cda15b8f640f
Alex Bedard-Reid <alexbedardreid@gmail.com>
2021-04-26 14:58:31 -0700

    Post-Screen Game UI implementation

---
+++ Assets/Scripts/UI/PostGameUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StarSalvager.UI.Elements;
+using TMPro;
+using UnityEngine.UI;
+
+namespace StarSalvager.UI
+{
+    public class PostGameUI : MonoBehaviour
+    {
+        //====================================================================================================================//
+        
+        [SerializeField]
+        private XPUIElementScrollView xpElementScrollview;
+
+        [SerializeField] private Slider xpSlider;
+        [SerializeField] private TMP_Text starCountText;
+
+        //====================================================================================================================//
+        
+        // Start is called before the first frame update
+        private void Start()
+        {
+
+        }
+
+        //====================================================================================================================//
+
+        private void SetupUI()
+        {
+            
+        }
+
+        //====================================================================================================================//
+        
+        
+    }
+}
