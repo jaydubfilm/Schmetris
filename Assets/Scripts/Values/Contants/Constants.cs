85e8b83da814ac6f9d4f20b99ef2a820d01cde51
Alex Bedard-Reid <alexbedardreid@gmail.com>
2021-07-14 13:04:39 -0700

    Fixed merge issues with Adding New Health system

--- Assets/Scripts/Values/Contants/Constants.cs
+++ Assets/Scripts/Values/Contants/Constants.cs
@@ -4,7 +4,7 @@ namespace StarSalvager.Values
 {
     public static class Constants
     {
-        public static readonly Version VERSION = new Version(1, 31, 4, 2);
+        public static readonly Version VERSION = new Version(1, 31, 4, 3);
 
         public const float VISIBLE_GAME_AREA = 0.55f;
 
