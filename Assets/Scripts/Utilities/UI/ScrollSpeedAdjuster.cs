62194358d4b9bbb1e486e29ca21c67f7b1708b23
Alex Bedard-Reid <alexbedardreid@gmail.com>
2021-07-14 10:05:37 -0700

    Improved scroll speed

---
+++ Assets/Scripts/Utilities/UI/ScrollSpeedAdjuster.cs
@@ -0,0 +1,31 @@
+﻿using StarSalvager.Values;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace StarSalvager.Utilities.UI
+{
+    [RequireComponent(typeof(ScrollRect))]
+    public class ScrollSpeedAdjuster : MonoBehaviour
+    {
+        private ScrollRect ScrollRect
+        {
+            get
+            {
+                if (_scrollRect == null)
+                    _scrollRect = GetComponent<ScrollRect>();
+                
+                return _scrollRect;
+            }
+        }
+        private ScrollRect _scrollRect;
+
+        private void OnEnable()
+        {
+#if UNITY_STANDALONE_WIN
+            ScrollRect.scrollSensitivity = Globals.WindowsScrollSpeed;
+#elif UNITY_STANDALONE_OSX
+            ScrollRect.scrollSensitivity = Globals.MacOSScrollSpeed;
+#endif
+        }
+    }
+}
